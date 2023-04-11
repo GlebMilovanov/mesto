93de354992dbb4c46afb9190eee6f0a2dca79db7
Gleb Milovanov <milgleb@gmail.com>
2023-04-11 19:46:06 +0300

    feat: disabled button style

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,8 @@
 const validationConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
+  submitButtonSelector: ".popup__submit-btn",
+  inactiveButtonClass: "popup__submit-btn_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
